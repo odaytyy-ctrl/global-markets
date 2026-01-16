d611e81944032ce7d60287eead6d6c2c6e30ae28
odaytyy-ctrl <odaytyy@gmail.com>
2026-01-17 01:39:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 function sayHello() {
   alert("مرحباً! JavaScript يعمل ✅");
 }
+function getGoldPrice() {
+  // سعر تجريبي (لاحقًا نربطه ببيانات حقيقية)
+  let price = 2350 + Math.floor(Math.random() * 20);
+  document.getElementById("goldPrice").innerText = price + " USD";
+}
