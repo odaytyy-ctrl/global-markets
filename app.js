6946826e7ef215d347c727e66618a6c04e63fc98
odaytyy-ctrl <odaytyy@gmail.com>
2026-01-17 03:41:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -23,3 +23,27 @@ async function loadPrices() {
 
 loadPrices();
 setInterval(loadPrices, 60000);
+async function loadPrices() {
+  try {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ripple,pax-gold&vs_currencies=usd"
+    );
+    const data = await res.json();
+
+    document.getElementById("gold").innerText =
+      data["pax-gold"].usd + " $";
+
+    document.getElementById("btc").innerText =
+      data.bitcoin.usd + " $";
+
+    document.getElementById("xrp").innerText =
+      data.ripple.usd + " $";
+
+  } catch (error) {
+    document.getElementById("gold").innerText = "❌";
+    document.getElementById("btc").innerText = "❌";
+    document.getElementById("xrp").innerText = "❌";
+  }
+}
+
+loadPrices();
